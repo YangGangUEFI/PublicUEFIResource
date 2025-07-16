44d720c1d8d9db6ad413cc75d1eff38781c12dd2
Yang Gang <yanggang.uefi@gmail.com>
2025-07-16 17:11:14 +0800

    Update run_ovmf-aarch64.bat
    Signed-off-by: Yang Gang <yanggang.uefi@gmail.com>

--- OvmfBat/run_ovmf-aarch64.bat
+++ OvmfBat/run_ovmf-aarch64.bat
@@ -1,14 +1,15 @@
+@REM truncate -s 64M Build/ArmVirtQemu-AARCH64/DEBUG_GCC5/FV/QEMU_EFI.fd
 chcp 65001
 "C:\Program Files\qemu\qemu-system-aarch64.exe" ^
   -M virt ^
   -cpu cortex-a57 ^
-  -pflash D:\OpenSource\edk2\Build\ArmVirtQemu-AARCH64\RELEASE_GCC5\FV\QEMU_EFI.fd ^
+  -pflash D:\OpenSource\edk2\Build\ArmVirtQemu-AARCH64\DEBUG_GCC5\FV\QEMU_EFI.fd ^
   -m 1024 ^
   -net none ^
   -serial stdio ^
   -hda fat:rw:EfiFiles ^
   -device virtio-rng-pci ^
   -device virtio-gpu-pci ^
-  -device qemu-xhci,id=usb ^
-  -device usb-tablet,id=input0,bus=usb.0,port=1 ^
-  -device usb-kbd,id=input1,bus=usb.0,port=2 ^
+  -device qemu-xhci,id=xhci ^
+  -device usb-mouse,bus=xhci.0 ^
+  -device usb-kbd,bus=xhci.0 ^
